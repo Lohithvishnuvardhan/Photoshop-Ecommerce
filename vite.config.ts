9c888949ab1c31e54d6d88c61df7e3e8d20e0948
Lohithvishnuvardhan <lohithvishnuvardhan@gmail.com>
2025-04-04 20:08:47 +0530

    Updated one

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: true, 
+    allowedHosts: ['promising-inherited-valued-joins.trycloudflare.com']
+  },
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
