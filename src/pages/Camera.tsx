1107fd14f5c46ed726a855c4b18b41e1f53a15a7
Lohithvishnuvardhan <lohithvishnuvardhan@gmail.com>
2025-04-19 12:28:46 +0530

    Update List

--- src/pages/Camera.tsx
+++ src/pages/Camera.tsx
@@ -1,6 +1,7 @@
 import { useCart } from '../context/Cartcontext';
 import { useNavigate } from 'react-router-dom';
 import { Star, Shield, Truck, Clock, Camera } from 'lucide-react';
+import toast from 'react-hot-toast';
 
 const cameras = [
   {
