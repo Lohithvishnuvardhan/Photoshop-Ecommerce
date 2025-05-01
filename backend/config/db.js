09b763e52d16b6857addcf3112f7551ecd1ff076
Lohithvishnuvardhan <lohithvishnuvardhan@gmail.com>
2025-05-01 20:27:42 +0530

    modify

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,12 +1,11 @@
-// config/db.js
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
-    console.log('MongoDB connected');
+    console.log('MongoDB connected successfully');
   } catch (error) {
-    console.error(error.message);
+    console.error('MongoDB connection error:', error.message);
     process.exit(1);
   }
 };
